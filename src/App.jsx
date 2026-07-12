368a0a9193f6442fb1cb248ccbfcfe7097c4bd6b
Gabriel Schwanke <gabriellschwanke@gmail.com>
2026-07-12 11:47:30 -0300

    feat: connect DataProvider to main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 function App() {
   return (
     <div className="bg-gray-900 text-white min-h-screen">
-      <h1 className="text-3xl font-bold text-center py-10">
-        Finance Day 💰
-      </h1>
+      <h1 className="text-3xl font-bold text-center py-10">Finance Day 💰</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { DataProvider } from './context/DataContext'
 import './index.css'
-import App from './App.jsx'
+import App from './App'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <DataProvider>
+      <App />
+    </DataProvider>
+  </StrictMode>
 )
